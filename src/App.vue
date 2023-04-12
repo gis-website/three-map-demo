a94f0f711825bc54acc6e816506495e8ef64a71c
TQtong <2733707740@qq.com>
2023-04-12 09:34:28 +0800

    Feat: three起步

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,42 @@
+<!--
+ * @Author: TQtong 2733707740@qq.com
+ * @Date: 2023-04-12 08:40:07
+ * @LastEditors: TQtong 2733707740@qq.com
+ * @LastEditTime: 2023-04-12 09:17:05
+ * @FilePath: \three-map-demo\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <ThreeMap />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from './components/HelloWorld.vue'
+import ThreeMap from './components/ThreeMap.vue'
 
 @Options({
   components: {
-    HelloWorld
+    ThreeMap
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="less">
+html,
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/components/ThreeMap.vue
@@ -0,0 +1,55 @@
+<!--
+ * @Author: TQtong 2733707740@qq.com
+ * @Date: 2023-04-12 08:45:56
+ * @LastEditors: TQtong 2733707740@qq.com
+ * @LastEditTime: 2023-04-12 09:31:56
+ * @FilePath: \three-map-demo\src\components\ThreeMap.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div id="myMap"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+camera.position.z = 5
+
+onMounted(() => {
+  const myMap = document.getElementById('myMap')
+  myMap!.appendChild(renderer.domElement)
+  animate()
+})
+
+function animate () {
+  requestAnimationFrame(animate)
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  renderer.render(scene, camera)
+}
+</script>
+
+<style scoped lang="less">
+#myMap {
+  width: 100%;
+  height: 100%;
+}
+</style>
