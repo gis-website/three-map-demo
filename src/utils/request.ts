29a6ed83a6505fc1ecaf1677908cab0c1ef0a911
TQtong <2733707740@qq.com>
2023-04-24 17:34:36 +0800

    Fix: optimize code

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,7 +2,7 @@
  * @Author: TQtong 2733707740@qq.com
  * @Date: 2023-04-11 09:51:07
  * @LastEditors: TQtong 2733707740@qq.com
- * @LastEditTime: 2023-04-11 09:51:46
+ * @LastEditTime: 2023-04-24 16:10:18
  * @FilePath: \echarts-map-demo\src\util\request.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,7 +15,7 @@ const service = axios.create({
 
 // 请求拦截
 service.interceptors.request.use(
-  (config) => {
+  (config: any) => {
     return config
   },
   (error: any) => {
